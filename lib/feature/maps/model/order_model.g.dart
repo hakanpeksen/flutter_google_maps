67a9999df22a470f38f65202c3b827b5627c41c3
Hakan Peksen <hknpksn@gmail.com>
2022-02-08 16:20:17 +0300

    order_model.g  fix

--- lib/feature/maps/model/order_model.g.dart
+++ lib/feature/maps/model/order_model.g.dart
@@ -11,16 +11,8 @@ OrderStatus _$OrderStatusFromJson(Map<String, dynamic> json) => OrderStatus(
       kurye: json['kurye'] as String?,
     );
 
-LocationModel _$LocationModelFromJson(Map<String, dynamic> json) =>
-    LocationModel(
+LocationModel _$LocationModelFromJson(Map<String, dynamic> json) => LocationModel(
       latitude: json['latitude'] as String?,
       longitude: json['longitude'] as String?,
       estimated: json['estimated'] as int?,
     );
-
-Map<String, dynamic> _$LocationModelToJson(LocationModel instance) =>
-    <String, dynamic>{
-      'latitude': instance.latitude,
-      'longitude': instance.longitude,
-      'estimated': instance.estimated,
-    };
